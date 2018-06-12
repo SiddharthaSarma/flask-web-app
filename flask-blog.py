e1bb4427cdb998c5089d95184a5fdad658af9013
siddharthasarma <sidduinevitable@gmail.com>
2018-06-12 22:26:02 +0530

    Added the functionality to loop over the content

--- flask-blog.py
+++ flask-blog.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'title': "Hello world",
+        'author': "Siddhartha",
+        'created_on': "April 21, 2018"
+    },
+    {
+        'title': "Hello world2",
+        'author': "Siddhartha",
+        'created_on': "April 21, 2018"
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
